6c4d96befe5fc929da171d96d6836d84c089cdc5
Sofia <117852305+Sofia777333@users.noreply.github.com>
2022-11-10 12:12:00 +0600

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Properties
+{
+
+
+    class Client
+    {
+        private int _age;
+
+        public string Name { get; set; } = "Default";
+
+        public int Age
+        {
+            get => _age;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("Возраст не может быть отрицательным");
+                _age = value;
+
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Properties
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+
+            client.Age = 56;
+
+            Console.WriteLine(client.Name);
+        }
+    }
+}
